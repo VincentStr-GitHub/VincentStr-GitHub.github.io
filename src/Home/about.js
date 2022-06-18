f6079d3bbc451df99155f0eae40652d54402c6d6
Vincent <vincent29art@gmail.com>
2022-06-18 11:59:28 +0200

    Website 18/06/22

--- src/Home/about.js
+++ src/Home/about.js
@@ -8,16 +8,7 @@ const About = (props) => {
         return (
             <div id="about" className="about" style={props.modeStyle}>
                 <p className="textItem">
-                    about
-                    preposition
-                    UK  /əˈbaʊt/ US  /əˈbaʊt/
-                    about preposition (CONNECTED WITH)
-    
-                    A1
-                    on the subject of, or connected with:
-                    What's that book about?
-                    a film about the Spanish Civil War
-                    We were talking/laughing about Sophie
+        
                 </p>
             </div>
         )
--- src/Projects/projectPage.js
+++ src/Projects/projectPage.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { useParams, Navigate } from "react-router-dom";
 import "./projectPage_Style.css"
+import ArrowIcon from "./svgs/arrowIcon";
 
+const GitHubLogo = require("./images/GitHub-Mark-64px.png")
 const pJSON = require("../Projects.json")
 
 const ProjectPage = (props) => {
@@ -15,7 +17,10 @@ const ProjectPage = (props) => {
 
         <div className="projectPageGrid" style={props.modeStyle}>
             <div className="project-item" id="item-Title"> <h1>{curProject.Name}</h1></div>
-            <div className="project-item" id="item-Preview"> <iframe className="project-preview" title={curProject.name + "Project Preview"} src= {curProject.Preview}></iframe></div>
+            <div className="project-item" id="item-Preview">
+                <div class="iframe-loading"><iframe className="project-preview" title={curProject.name + "Project Preview"} src={curProject.Preview}></iframe></div>
+                <div className="project-links"> <a href=""><img src={GitHubLogo} /></a> <div id="codeIcon"><ArrowIcon /></div> </div>
+            </div>
             <div className="project-item"> <p>Text</p> </div>
             <div className="project-item" id="item-Tech"> TechUsed </div>
             <div className="project-item"> {curProject.DateCreated}</div>
