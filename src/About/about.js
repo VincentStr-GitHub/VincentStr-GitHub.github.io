a62719afbb65d56c5941f2f987655def940fdb9d
Vincent <vincent29art@gmail.com>
2022-06-22 15:06:20 +0200

    Website 22/06/22

--- src/About/about.js
+++ src/About/about.js
@@ -1,13 +1,21 @@
 import React from "react"
 import "./aboutStyle.css"
-const About = (props) =>{
+const About = (props) => {
     return (
-        <div className="aboutPage">
-        <div className="textAbout" style={props.modeStyle}>
-            <p> 
-                Hello, <br /> I am Vincent Strigel, a Computer Science studtent at the Technische Hochschule Ulm, currently in my 2nd semseter. 
-            </p>
-        </div>
+        <div style={props.modeStyle} className="aboutPage">
+            <div className="textAbout" >
+                <h1>  <b>Hi, <br /> I’m Vincent. </b> </h1>
+                <p>
+                   
+                    I am currently in my 2nd semester studying Computer Science at the Technische Hochschule Ulm.<br /> <br />
+
+                    I am passionate about everything computer related, from the internal hardware to the software that makes it all work. 
+                    My focus lies with writing code to create programs that solve problems and make life easier. 
+                    I’ve worked the longest with writing JavaScript code, and using frameworks such as React, but I’ve also used Java and SQL, 
+                    especially for my studies. Learning new things is always facisting, I want to continue to expand my knowledge with new 
+                    languages and technologies but also deepen my understanding of the ones I already have experience with.
+                </p>
+            </div>
         </div>
     )
 }
--- src/Contact/contact.js
+++ src/Contact/contact.js
@@ -2,15 +2,17 @@ import React from "react"
 import "./contactStyle.css"
 import MailIcon from "./mailSvg"
 
-const Contact = (props) =>{
+const Contact = (props) => {
     return (
-        <div className="contactPage">
-        <div className="textContact" style={props.modeStyle}>
-           <h1>Contact</h1>
-           <h2>Email: vincentstriegel@gmail.com</h2>
-           <div className="mailIcon">  <a href="mailto:vincentstriegel@gmail.com">< MailIcon /></a> </div>
-          
-        </div>
+        <div style={props.modeStyle} className="contactPage">
+            <div className="textContact">
+                <h1>Contact me via E-mail or Phone</h1>
+                <div id="mailBlock">
+                    <h2>Email: vincentstriegel@gmail.com</h2>
+                    <div className="mailIcon">  <a href="mailto:vincentstriegel@gmail.com">< MailIcon /></a> </div>
+                </div>
+                <h2>Phone: +49 152 22186057</h2>
+            </div>
         </div>
     )
 }
