2103ff92506aeb9f632d5a24a4e480b172b16f49
Vincent <vincent29art@gmail.com>
2022-08-23 15:57:11 +0200

    Website 23/08

--- src/Projects/allProjectsGrid.js
+++ src/Projects/allProjectsGrid.js
@@ -3,7 +3,7 @@ import CreateProjects from "./createProjects"
 import "./allProjectsGridStyle.css"
 
 
-const listProjects = [0,1,2,3,4,5,6,7,8]
+const listProjects = [0,1,2,3,4,5,9,6,7,8]
 
 function AllProjectsGrid(props) {
     return (
