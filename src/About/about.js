fbae83ff356c4c842734afe108abcce7dd57f5dd
Vincent <vincent29art@gmail.com>
2022-08-25 15:59:35 +0200

    Website 25/08

--- src/About/about.js
+++ src/About/about.js
@@ -7,11 +7,11 @@ const About = (props) => {
                 <h1>  <b>Hi, <br /> I’m Vincent. </b> </h1>
                 <p>
                    
-                    I am currently in my 2nd semester studying Computer Science at the Technische Hochschule Ulm.<br /> <br />
+                    I am currently in my 3rd semester studying Computer Science at the Technische Hochschule Ulm.<br /> <br />
 
                     I am passionate about everything computer related, from the internal hardware to the software that makes it all work. 
                     My focus lies with writing code to create programs that solve problems and make life easier. 
-                    I’ve worked the longest with writing JavaScript code, and using frameworks such as React, but I’ve also used Java and SQL, 
+                    I’ve worked the longest with writing JavaScript code, and using frameworks such as React and Angular, but I’ve also used Java and SQL, 
                     especially for my studies. Learning new things is always facisting, I want to continue to expand my knowledge with new 
                     languages and technologies but also deepen my understanding of the ones I already have experience with.
                 </p>
